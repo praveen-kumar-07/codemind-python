896c6080aa4913b5447553a1281410d4da3f2368
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2023-06-01 22:45:05 -0700

    n=int(input()) ls=list(map(int,input().split())) c=0 i=0 m=0 while(i<n):     if(ls[i]==1):         c+=1         if(c>m):             m=c     else:         c=0     i+=1 print(m)

---
+++ Max_Consecutive_Ones.py
@@ -0,0 +1,14 @@
+n=int(input())
+ls=list(map(int,input().split()))
+c=0
+i=0
+m=0
+while(i<n):
+    if(ls[i]==1):
+        c+=1
+        if(c>m):
+            m=c
+    else:
+        c=0
+    i+=1
+print(m)
