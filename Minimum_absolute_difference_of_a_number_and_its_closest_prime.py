ad78b134340c68c7282fa11d8dce12187265140e
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2023-08-23 21:07:37 +0530

    def prime(n:int)->int:     k=int(n**0.5+1)     for i in range(2,k):         if(n%i==0):             return 0     return 1 def prev_prime(n):     while(n):         if(prime(n)):             break         else:             n-=1     return n def next_prime(n):     while(n):         if(prime(n)):             break         else:             n+=1     return n n=int(input()) fn=prev_prime(n) bn=next_prime(n) if(n-fn>bn-n):     print(bn-n) else:     print(n-fn)

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -1,21 +1,27 @@
-def prime(n):
+def prime(n:int)->int:
     k=int(n**0.5+1)
     for i in range(2,k):
         if(n%i==0):
-            return False
-    return True
-def next_prime(n):
-    while(n):
-        if(prime(n)):
-            return n
-        n+=1
+            return 0
+    return 1
 def prev_prime(n):
     while(n):
         if(prime(n)):
-            return n
-        n-=1
+            break
+        else:
+            n-=1
+    return n
+def next_prime(n):
+    while(n):
+        if(prime(n)):
+            break
+        else:
+            n+=1
+    return n
 n=int(input())
-if(next_prime(n)-n<n-prev_prime(n)):
-    print(next_prime(n)-n)
+fn=prev_prime(n)
+bn=next_prime(n)
+if(n-fn>bn-n):
+    print(bn-n)
 else:
-    print(n-prev_prime(n))
+    print(n-fn)
