dc71c565519554d80682002cb8106f88a9b8ff83
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2023-08-24 20:27:23 +0530

    def prime(n:int)->int:     if(n==1):         return 0     k=int(n**0.5+1)     for i in range(2,k):         if(n%i==0):             return 0     return 1 def next_prime(n):     if(prime(n)==1):         return n     else:         return next_prime(n+1) a=int(input()) b=int(input()) n=next_prime(a+b+1) print(n-a-b)

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,18 +1,18 @@
-def prime(n):
-    k=int(n**0.5+1)
+def prime(n:int)->int:
     if(n==1):
-        return False
+        return 0
+    k=int(n**0.5+1)
     for i in range(2,k):
         if(n%i==0):
-            return False
-    return True
+            return 0
+    return 1
 def next_prime(n):
-    while(n):
-        if(prime(n)):
-            return n
-        n+=1
+    if(prime(n)==1):
+        return n
+    else:
+        return next_prime(n+1)
 a=int(input())
 b=int(input())
-c=next_prime(a+b+1)
-print(c-a-b)
-            
+n=next_prime(a+b+1)
+print(n-a-b)
+    
