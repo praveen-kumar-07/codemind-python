8da941a2503536770388e24ea3882859b8a4a1ed
Praveen Kumar <119918367+praveen-kumar-07@users.noreply.github.com>
2023-08-24 20:35:11 +0530

    def prime(n:int)->int:     k=int(n**0.5+1)     if(n==1):         return 0     for i in range(2,k):         if(n%i==0):             return 0     return 1 a=int(input()) k=str(a) if(prime(a) and prime(int(k[::-1]))):     print("circular prime") elif(prime(a)):     print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,16 +1,16 @@
-def prime(n):
+def prime(n:int)->int:
     k=int(n**0.5+1)
+    if(n==1):
+        return 0
     for i in range(2,k):
         if(n%i==0):
-            return False
-    return True
-n=input()
-if(prime(int(n))):
-    if(prime(int(n[::-1]))):
-        print("circular prime")
-    else:
-        print("prime but not a circular prime")
+            return 0
+    return 1
+a=int(input())
+k=str(a)
+if(prime(a) and prime(int(k[::-1]))):
+    print("circular prime")
+elif(prime(a)):
+    print("prime but not a circular prime")
 else:
     print("not prime")
-    
-    
